92a19513eee0bd655ca1a063d1bce7aa55a9ad0b
M-Wiz <themightysorcererofthelands@gmail.com>
2023-12-21 14:49:36 +0000

    Commits

--- Introduction_to_Cryptohack/Hex.py
+++ Introduction_to_Cryptohack/Hex.py
@@ -5,3 +5,8 @@
 # In Python, the bytes.fromhex() function can be used to convert hex to bytes. The .hex() instance method can be called on byte strings to get the hex representation.
 
 
+hex_string = "63727970746f7b596f755f77696c6c5f62655f776f726b696e675f776974685f6865785f737472696e67735f615f6c6f747d"
+
+flag = bytes.fromhex(hex_string)
+
+print(flag.decode('utf-8'))
